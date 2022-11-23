5aca31c34ee4af4948eecd4780f617e4fe347001
henil1711 <henilkapadiya90@gmail.com>
2022-11-23 17:51:54 +0530

    new assinment

---
+++ henil2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    float principal, rate, time, simpel_interest, compound_interest;
+
+    printf(" Enter the value of principal : ");
+    scanf("%f", &principal);
+    printf("got it! /n");
+    printf("Now, Enter the value of rate :");
+    scanf("%f", &rate);
+    printf("got it! /n");
+    printf ("Enter the time : ");
+    scanf("%f", &time);
+
+simpel_interest = principal * rate / 100 * time;
+compound_interest = principal * pow ((1 + rate/100), time)-principal;
+
+ printf("your simpel interest is %f", simpel_interest);
+ printf("your compound interest is %f", compound_interest);
+
+ return 0;
+
+    
+
+
+}
