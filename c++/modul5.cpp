e12aca3a2dfaa2948f21c092bf6aa2bf4d6bd2c7
henil1711 <118728542+henil1711@users.noreply.github.com>
2023-01-06 14:42:05 +0530

    Delete c++ directory

--- c++/modul5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n\n";
-    cout << "| Welco me to Tops Technologies |\n";
-    cout << "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _  \n";
-    return 0;
-}
